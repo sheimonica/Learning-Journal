66f12f5165a3dcdbc9a446a57b51d0ff100d70f7
Sheila Monica <sheimonica98@gmail.com>
2022-06-14 11:42:08 +0800

    update

--- phase_0/nongraded_challenge/h8dsft_converter.py
+++ phase_0/nongraded_challenge/h8dsft_converter.py
@@ -1,12 +1,12 @@
 def kelvin_celcius ():
     temp_check = ["kelvin", "celcius"]
     temperature = input("Please input the temperature: ")
-    
-    while type(temperature) != int or type(temperature) != float:
-        print ("That is not a number!")
+
+    while temperature != float:
+        print ("wrong temperature!")
         temperature = input("Please input the temperature: ")
 
-    print ("Thank you!")
+    print ("Thank you")
     print ("Is this temperature in kelvin or celcius?")
     temp_type = input("Please input 'kelvin' for kelvin and 'celcius' for celcius: ")
     temp_type.lower()
@@ -14,8 +14,7 @@ def kelvin_celcius ():
     while temp_type not in temp_check:
         print ("wrong temperature type!")
         print ("Is this temperature in kelvin or celcius?")
-        temp_type = input("Please input 'kelvin' for kelvin and 'celcius' for celcius: ")
-        temp_type.lower()
+        temp_type = input("Please input 'kelvin' for kelvin and 'celcius' for celcius: ").lower()
 
     if temp_type in temp_check:
         print (f"The temperature you entered was {temperature}")
