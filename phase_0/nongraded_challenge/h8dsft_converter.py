b79eb99f340bcc934e963ac311d7b4aa8f4ce786
Sheila Monica <sheimonica98@gmail.com>
2022-06-14 13:40:19 +0800

    update 6/14/2022 1:40pm

--- phase_0/nongraded_challenge/h8dsft_converter.py
+++ phase_0/nongraded_challenge/h8dsft_converter.py
@@ -1,6 +1,8 @@
+#basic info
 temp_type = ""
-temp_check = ["kelvin", "celcius", ""]
+temp_check = ["kelvin", "celcius", "fahrenheit"]
 
+#convert kelvin-celcius dan sebaliknya
 def kelvin_celcius ():
     if temp_type == "celcius":
         print (f"The temperature you entered was {temperature}")
@@ -19,6 +21,7 @@ def kelvin_celcius ():
         print (f"The temperature was in {temp_type}")
         print (f"no conversion happened")
 
+#convert ke fahrenheit dari celcius atau kelvin
 def convert_fahrenheit():
     if temp_type == "celcius":
         print (f"The temperature you entered was {temperature}")
@@ -30,13 +33,14 @@ def convert_fahrenheit():
         print (f"The temperature you entered was {temperature}")
         print (f"The temperature was in {temp_type}")
         new_temp = (9/5)*(temperature-273) + 32
-        print (f"The new temperature in celcius is {new_temp}°F ")
+        print (f"The new temperature in kelvin is {new_temp}°F ")
     
     else:
         print (f"The temperature you entered was {temperature}")
         print (f"The temperature was in {temp_type}")
         print (f"no conversion happened")
 
+#convert fahrenheit ke celcius atau kelvin
 def converting_fahrenheit():
     if desired_temp == "celcius":
         print (f"The temperature you entered was {temperature}")
@@ -48,13 +52,14 @@ def converting_fahrenheit():
         print (f"The temperature you entered was {temperature}")
         print (f"The temperature was in {temp_type}")
         new_temp = (5/9)*(temperature - 32) + 273.15
-        print (f"The new temperature in celcius is {new_temp}°F ")
+        print (f"The new temperature in kelvin is {new_temp}°F ")
     
     else:
         print (f"The temperature you entered was {temperature}")
         print (f"The temperature was in {temp_type}")
         print (f"no conversion happened")
 
+#decide which conversion to do
 def decision():
     if temp_type == "fahrenheit":
         converting_fahrenheit()
@@ -65,11 +70,13 @@ def decision():
     else:
         kelvin_celcius()
 
+#input
 temperature = float(input("Please input the temperature: "))
 
 print ("Is this temperature in kelvin, celcius, or fahrenheit?")
 temp_type = input("Please input 'kelvin' for kelvin, 'celcius' for celcius, and 'fahrenheit' for fahrenheit: ").lower()
     
+#correcting temperature
 while temp_type not in temp_check:
     print ("wrong temperature type!")
     print ("Is this temperature in kelvin or celcius?")
@@ -77,10 +84,12 @@ while temp_type not in temp_check:
 
 print ("Do you want to convert this into kelvin, celcius, or fahrenheit?")
 desired_temp = input("Please input 'kelvin' for kelvin, 'celcius' for celcius, and 'fahrenheit' for fahrenheit: ").lower()
-    
+
+#correcting desired temperature    
 while desired_temp not in temp_check:
     print ("wrong temperature type!")
     print ("Do you want to convert this into kelvin, celcius, or fahrenheit?")
     desired_temp = input("Please input 'kelvin' for kelvin, 'celcius' for celcius, and 'fahrenheit' for fahrenheit: ").lower()
-    
+
+#decide conversion    
 decision()
